3946ad590f63ce1ef2f57b57bf5406a9febd0ec8
Luis E. Sánchez P <42817881+lesanpi@users.noreply.github.com>
2019-02-25 14:48:31 -0400

    Add files via upload

---
+++ precache-manifest.c4e35d4b0de0a4807297172e2d863207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c06f5e5a2ae47006c0b77be4de6a2077",
+    "url": "/static/media/logo.c06f5e5a.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6a81aaee64d59cd5d3c4",
+    "url": "/static/js/main.6a81aaee.chunk.js"
+  },
+  {
+    "revision": "3ad84101692611e66582",
+    "url": "/static/js/2.3ad84101.chunk.js"
+  },
+  {
+    "revision": "6a81aaee64d59cd5d3c4",
+    "url": "/static/css/main.fc79c512.chunk.css"
+  },
+  {
+    "revision": "0f77d08976851480b42c7207bccb5d58",
+    "url": "/index.html"
+  }
+];
